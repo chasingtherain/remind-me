3cec1e12628448ffc9aaa5740864a2417f2e8292
ChasingRain <junpeng94@gmail.com>
2022-07-08 10:53:10 +0800

    got time and date from api, saved it as state and render on Form component; added spinner component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import Navbar from './components/Navbar';
 import Form from './components/Form';
 import useGlobalContext from './hooks/useGlobalContext';
+import Spinner from './components/Spinner';
+
 
 function App() {
-    const {region, setRegion} = useGlobalContext()
-
+    const {region} = useGlobalContext()
 
     return (
       <>
         <Navbar/>
-        {/* form will only load when data is received */}
-        {region && <Form region={region} setRegion={setRegion}/>}
+        {/* form will only load when data is received and in the interim, spinner will be shown*/}
+        {region && <Form/>}
+        {!region && <Spinner/>}
       </>
     );
 }
--- src/contexts/GlobalContext.js
+++ src/contexts/GlobalContext.js
@@ -7,34 +7,64 @@ export const GlobalContextProvider = ({children}) => {
 
     const [region, setRegion] = useState("")
     const [timezone, setTimezone] = useState("")
+    const [currentTime, setCurrentTime] = useState("")
+    const selectTimezone = (e)=> {
+        setTimezone(e.target.value)
+    }
 
+    // fetch time zone info
     useEffect(()=> {
         fetchTimezone()
-        fetchInfo()
       // eslint-disable-next-line react-hooks/exhaustive-deps
       },[])
+
+    // fetch time of a city
+    useEffect(()=> {
+        const fetchInfo = async () => {
+            const response = await fetch(`${TIMEZONE_API_BASE_URL}/timezone/${timezone}`)
+            response.json()
+                .then(data => {
+                    let localeTime = data.datetime
+                    // splice api's date time data to get hh:mm:ss
+                    console.log(localeTime);
+                    let tIndex = localeTime.indexOf("T")
+                    let lastDot = localeTime.lastIndexOf(".")
+                    console.log(tIndex, lastDot)
+                    setCurrentTime(localeTime.slice(tIndex+1,lastDot))
+            })
+        }
+        fetchInfo()
+    },[timezone])
+
   
       const fetchTimezone = async () => {
         const response = await fetch(`${TIMEZONE_API_BASE_URL}/timezone`)
-        const timezones = response.json()
+        response.json()
           .then(data => setRegion(data))
-        console.log(timezones);
+        // console.log(timezones);
   
       }
-      const fetchInfo = async () => {
-        const response = await fetch(`${TIMEZONE_API_BASE_URL}/timezone/Asia/Singapore`)
-        const info = response.json()
-          .then(data => console.log(data))
-        console.log(info);
-      }
+    //   const fetchInfo = async () => {
+    //     console.log(timezone);
+    //     const response = await fetch(`${TIMEZONE_API_BASE_URL}/timezone/${timezone}`)
+    //     // const response = await fetch(`${TIMEZONE_API_BASE_URL}/timezone/Asia/Singapore`)
+    //     response.json()
+    //       .then(data => {
+    //         // setCurrentTime(data.datetime)
+    //         console.log(data.datetime);
+    //     })
+    //   }
 
     return (
     <GlobalContext.Provider value={{
+        currentTime,
         region, 
         timezone,
         TIMEZONE_API_BASE_URL,
+        setCurrentTime,
         setRegion,
         setTimezone,
+        selectTimezone,
         }}>
         {children}
     </GlobalContext.Provider>
